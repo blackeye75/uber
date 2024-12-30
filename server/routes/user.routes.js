437a604e8c6fbc49b2ded7f01fc6fa1a16ebcf89
blackeye75 <priyanshuraj22275@gmail.com>
2024-12-30 17:04:24 +0530

    started

--- server/routes/user.routes.js
+++ server/routes/user.routes.js
@@ -2,8 +2,10 @@ const express = require('express')
 
 const router = express.Router();
 
-const {body}=require("express-validator")
+const { body } = require("express-validator")
 
-const userController=
+const userController = require("../controllers/user.controller")
 
-module.exports=router;
+router.post('/register', [body('email').isEmail().withMessage("Invalid Email"), body('fullname.firstname').isLength({ min: 3 }).withMessage('First name must 4 3 characte long'), body('password').isLength({ min: 6 }).withMessage('Passsword must be 6 character long')], userController.registerUser)
+
+module.exports = router;
