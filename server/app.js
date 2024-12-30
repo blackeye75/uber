7d047fae3b901369f8ae8b0e3e7fca27ee986632
blackeye75 <priyanshuraj22275@gmail.com>
2024-12-30 19:28:25 +0530

    add user registration functionality with validation and token generation

--- server/app.js
+++ server/app.js
@@ -10,8 +10,15 @@ app.use(cors())
 const db = require("./database/db")
 db();
 
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }));
+
+const userRoutes = require("./routes/user.routes")
+
 app.get("/", (req, res) => {
     res.send("Hello world from server")
 })
+
+app.use("/users", userRoutes);
 
 module.exports = app; 
