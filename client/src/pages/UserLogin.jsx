b08d2837dfa8d13cf99c981e1430a90f14a065dd
blackeye75 <priyanshuraj22275@gmail.com>
2025-01-27 13:49:35 +0530

    loginform initiated

--- client/src/pages/UserLogin.jsx
+++ client/src/pages/UserLogin.jsx
@@ -1,9 +1,41 @@
-import React from 'react'
+import React from "react";
 
 const UserLogin = () => {
-  return (
-    <div>UserLogin</div>
-  )
-}
+    return (
+        <div className="p-4">
+            <div>
+                <img
+                    className="w-20  mb-4"
+                    src="https://upload.wikimedia.org/wikipedia/commons/thumb/c/cc/Uber_logo_2018.png/1200px-Uber_logo_2018.png"
+                    alt=""
+                />
+                <form action="">
+                    <h3 className="text-lg font-medium mb-2">Email Your Email</h3>
+                    <input
+                        type="email"
+                        className="bg-[#eeeeee] mb-5 rounded px-4 py-2 border  w-full text-lg  placeholder:text-base"
+                        required
+                        placeholder="email@example.com"
+                    />
+                    <h3 className="text-lg font-medium mb-2">Enter Password</h3>
+                    <input
+                        type="password"
+                        className="bg-[#eeeeee] rounded px-4 py-2 border  mb-7 w-full text-lg  placeholder:text-base"
+                        required
+                        placeholder="password"
+                    />
+                    <button className="bg-black text-white font-semibold rounded mb-7 w-full px-4 py-2">
+                        Login
+                    </button>
+                </form>
+            </div>
+            <div>
+                <button className="bg-[#10b461] flex items-center justify-center text-white font-semibold mb-5 rounded-lg px-4 py-2 w-full text-lg placeholder:text-base">
+                    Sign in as Captian
+                </button>
+            </div>
+        </div>
+    );
+};
 
-export default UserLogin
+export default UserLogin;
