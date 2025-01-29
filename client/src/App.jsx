d677b108396a771dc5e99a61e19081dd09a6c10e
blackeye75 <priyanshuraj22275@gmail.com>
2025-01-30 05:21:50 +0530

    refactor: clean up code formatting and improve readability in App, Home, and UserLogin components

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,7 +3,7 @@ import { Route, Routes } from 'react-router-dom'
 import Home from './pages/Home'
 import UserLogin from './pages/UserLogin'
 import UserSignup from './pages/UserSignup'
-import CaptianSignup from './pages/CaptianSignup' 
+import CaptianSignup from './pages/CaptianSignup'
 import CaptianLogin from './pages/CaptianLogin'
 
 function App() {
@@ -11,13 +11,13 @@ function App() {
 
   return (
     <div>
-     <Routes>
-      <Route path="/" element={<Home />} /> 
-      <Route path="/login" element={<UserLogin />} /> 
-      <Route path="/signup" element={<UserSignup />} /> 
-      <Route path="/captian-login" element={<CaptianLogin />} /> 
-      <Route path="/captian-signup" element={<CaptianSignup />} /> 
-     </Routes>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<UserLogin />} />
+        <Route path="/signup" element={<UserSignup />} />
+        <Route path="/captian-login" element={<CaptianLogin />} />
+        <Route path="/captian-signup" element={<CaptianSignup />} />
+      </Routes>
     </div>
   )
 }
--- client/src/pages/UserLogin.jsx
+++ client/src/pages/UserLogin.jsx
@@ -1,41 +1,45 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const UserLogin = () => {
-    return (
-        <div className="p-4">
-            <div>
-                <img
-                    className="w-20  mb-4"
-                    src="https://upload.wikimedia.org/wikipedia/commons/thumb/c/cc/Uber_logo_2018.png/1200px-Uber_logo_2018.png"
-                    alt=""
-                />
-                <form action="">
-                    <h3 className="text-lg font-medium mb-2">Email Your Email</h3>
-                    <input
-                        type="email"
-                        className="bg-[#eeeeee] mb-5 rounded px-4 py-2 border  w-full text-lg  placeholder:text-base"
-                        required
-                        placeholder="email@example.com"
-                    />
-                    <h3 className="text-lg font-medium mb-2">Enter Password</h3>
-                    <input
-                        type="password"
-                        className="bg-[#eeeeee] rounded px-4 py-2 border  mb-7 w-full text-lg  placeholder:text-base"
-                        required
-                        placeholder="password"
-                    />
-                    <button className="bg-black text-white font-semibold rounded mb-7 w-full px-4 py-2">
-                        Login
-                    </button>
-                </form>
-            </div>
-            <div>
-                <button className="bg-[#10b461] flex items-center justify-center text-white font-semibold mb-5 rounded-lg px-4 py-2 w-full text-lg placeholder:text-base">
-                    Sign in as Captian
-                </button>
-            </div>
-        </div>
-    );
+  return (
+    <div className="p-4">
+      <div>
+        <img
+          className="w-20  mb-4"
+          src="https://upload.wikimedia.org/wikipedia/commons/thumb/c/cc/Uber_logo_2018.png/1200px-Uber_logo_2018.png"
+          alt=""
+        />
+        <form action="">
+          <h3 className="text-lg font-medium mb-2">Email Your Email</h3>
+          <input
+            type="email"
+            className="bg-[#eeeeee] mb-5 rounded px-4 py-2 border  w-full text-lg  placeholder:text-base"
+            required
+            placeholder="email@example.com"
+          />
+          <h3 className="text-lg font-medium mb-2">Enter Password</h3>
+          <input
+            type="password"
+            className="bg-[#eeeeee] rounded px-4 py-2 border  mb-7 w-full text-lg  placeholder:text-base"
+            required
+            placeholder="password"
+          />
+          <button className="bg-black text-white font-semibold rounded mb-7 w-full px-4 py-2">
+            Login
+          </button>
+          <p className="text-blue-500 text-center mb-3" >New Here?
+            <Link to="/signup" className="text-black" > Create new Account</Link>
+          </p>
+        </form>
+      </div>
+      <div>
+        <button className="bg-[#10b461] flex items-center justify-center text-white font-semibold mb-5 rounded-lg px-4 py-2 w-full text-lg placeholder:text-base">
+          Sign in as Captian
+        </button>
+      </div>
+    </div>
+  );
 };
 
 export default UserLogin;
